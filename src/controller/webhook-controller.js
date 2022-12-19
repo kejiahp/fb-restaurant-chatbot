10ae75924adf69f8026424da5096f00137a6321f
kejiahp <popoolakejiah@gmail.com>
2022-12-19 22:06:34 +0100

    updated message handlign

--- src/controller/webhook-controller.js
+++ src/controller/webhook-controller.js
@@ -1,26 +1,7 @@
+const { handleMessage, handlePostback } = require('../../utils/messaging_utils');
+
 require('dotenv').config()
 
-const postWebhook = (req, res) => {  
-
-    let body = req.body;
-  
-    if (body.object === 'page') {
-  
-      body.entry.forEach(function(entry) {
-  
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-        
-      });
-
-      res.status(200).send('EVENT_RECEIVED');
-  
-    } else {
-      res.sendStatus(404);
-    }
-  
-  };
-  
 const getWebhook = (req, res) => {
     
     /** UPDATE YOUR VERIFY TOKEN **/
@@ -44,7 +25,64 @@ const getWebhook = (req, res) => {
     else{
       res.sendStatus(403)
     }
+};
+
+/*
+  {
+  "object":"page",
+  "entry":[
+    {
+      "id":"<PAGE_ID>",
+      "time":1458692752478,
+      "messaging":[
+        {
+          "sender":{
+            "id":"<PSID>"
+          },
+          "recipient":{
+            "id":"<PAGE_ID>"
+          },
+
+          ...
+        }
+      ]
+    }
+  ]
+}
+*/
+
+const postWebhook = (req, res) => {  
+
+    let body = req.body;
+  
+    if (body.object === 'page') {
+      
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      body.entry.forEach(function(entry) {
+  
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+
+        //get the sender psid - page scoped id
+        let sender_psid = webhook_event.sender.id
+        console.log('Sender PSID: '+sender_psid)
+        
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message)
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback)
+        }
+      });
+
+      res.status(200).send('EVENT_RECEIVED');
+  
+    } else {
+      res.sendStatus(404);
+    }
+  
   };
+  
 
 module.exports = {
   postWebhook,
---
+++ utils/messaging_utils.js
@@ -0,0 +1,65 @@
+const request = require('request')
+
+//psid - page scoped id
+
+// Sends response messages via the Send API
+const callSendAPI = (sender_psid, response) => {
+    // Construct the message body
+    let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    uri: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {"access_token": process.env.PAGE_ACCESS_TOKEN},
+    method: "POST",
+    json: request_body
+  }, (err, res, body) => {
+    if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+  })
+}
+
+// Handles messages events
+const handleMessage = (sender_psid, received_message) => {
+    let response
+
+    if (received_message.text) {    
+
+        // Create the payload for a basic text message
+        response = {
+          "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+    else if (received_message.attachments) {
+        //get the payload.url
+        let attachment_urls = []
+
+        received_message.attachments.forEach(item =>{
+            attachment_urls.push(item.payload.url)
+        })
+    }
+
+      // Sends the response message
+    callSendAPI(sender_psid, response);    
+}
+
+// Handles messaging_postbacks events
+const handlePostback = (sender_psid, received_postback) => {
+    
+}
+
+
+
+module.exports = {
+    handleMessage,
+    handlePostback,
+    callSendAPI
+}
